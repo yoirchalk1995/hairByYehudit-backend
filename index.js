79dbd240a766c5c6309b50240c1d8ddd9708ba5f
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-26 18:15:46 +0200

    created and exported db connection

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const helmet = require("helmet");
+const mysql = require("mysql2/promise");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "yoirchalk1995",
+  database: "hair_by_yehudit",
+});
+
+db.getConnection()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(`error conecting to db; ${err}`);
+    process.exit(1);
+  });
 
 const app = express();
 
@@ -8,3 +25,5 @@ app.use(helmet());
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
+exports = db;
