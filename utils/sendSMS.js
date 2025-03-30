af1e30080bbf75d081d8ea223a11c7a19cfdf3a7
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-30 08:58:13 +0300

    sensSMS working.

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -1,23 +1,9 @@
 const twilio = require("twilio");
-const http = require("http");
-const axios = require("axios");
-const fs = require("fs");
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const accountSid = "AC486373f7b5bfc1189f172d3e7137558a";
-const authToken = "0cf5bb83a135e0661234f9b29baca54c";
-
-const customAgent = new http.Agent({
-  ca: fs.readFileSync(
-    "C:\\Users\\יהודית\\Documents\\2023 techloq bundle certificate.crt"
-  ),
-});
-
-const customHttpClient = axios.create({ httpAgent: customAgent });
+const authToken = "61a0a278e5bf6f6a0c2a7a82da94a94e";
 
 const client = twilio(accountSid, authToken);
-client.httpClient = { request: customHttpClient.request };
 
 const otp = Math.floor(100000000 * Math.random());
 
