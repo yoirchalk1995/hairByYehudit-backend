9a7d4a05a968b27835ea02be7cd9cc43c5d87960
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-30 18:03:29 +0300

    added dynamic port

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.use(helmet());
 app.use(express.json());
 app.use("/users", users);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
 
 module.exports = app;
