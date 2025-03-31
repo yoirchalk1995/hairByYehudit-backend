8cb6dcd0c9a7a0e7a1e6e4c1d2e41188209316d8
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-31 21:02:38 +0300

    added POST method w/ basic logic

--- routes/verification.js
+++ routes/verification.js
@@ -1,3 +1,31 @@
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+
+const db = import("../startup/db");
 const router = require("express").Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validateVerification(req.body);
+  if (error) return res.status(400).send(err.details[0].message);
+  const { userId, otp } = req.body;
+  try {
+    const [rows] = await db.query(
+      "SELECT * FROM verification WHERE user_id = ?",
+      [userId, otp]
+    );
+    console.log(rows);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const validateVerification = function (verification) {
+  const schema = Joi.object({
+    userId: Joi.number().positive().required(),
+    otp: Joi.number().required().min(100000000),
+  });
+
+  return schema.validate(verification);
+};
 
 module.exports = router;
