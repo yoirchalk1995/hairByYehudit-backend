78f9c73bbdd9071ee791f78b638420e327595a14
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-01 23:02:22 +0300

    added services route

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const helmet = require("helmet");
+
+const services = require("./routes/services");
 const users = require("./routes/users");
 const verification = require("./routes/verification");
+
 require("./startup/db");
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
+
+app.use("/services", services);
 app.use("/users", users);
 app.use("/verification", verification);
 
