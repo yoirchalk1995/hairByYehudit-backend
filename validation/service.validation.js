f8f53d1ec267ef245cf7cda2bf00f3591ef412eb
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-02 17:14:56 +0300

    renamed verification to validation

---
+++ validation/service.validation.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+
+const validateService = function (service) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(20).required(),
+    lengthInMinutes: Joi.number().positive().required(),
+    price: Joi.number().positive().max(300).required(),
+    inPerson: Joi.boolean().required(),
+  });
+
+  return schema.validate(service);
+};
