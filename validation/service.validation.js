cbdb410bd014ffe4e617923978aa9dc74cdd10f3
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-02 19:19:51 +0300

    changed prop lengthInMinutes to lengthInMin as used elsewhere

--- validation/service.validation.js
+++ validation/service.validation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const validateService = function (service) {
   const schema = Joi.object({
     name: Joi.string().min(5).max(20).required(),
-    lengthInMinutes: Joi.number().positive().required(),
+    lengthInMin: Joi.number().positive().required(),
     price: Joi.number().positive().max(300).required(),
     inPerson: Joi.boolean().required(),
   });
