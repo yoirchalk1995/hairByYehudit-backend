8b1853873231038d81d2eedfc483d8c32fda93b4
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-06 00:25:59 +0300

    added logging to winston

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,7 @@
+const logger = require("../utils/logger");
+
 module.exports = function (err, req, res, next) {
+  logger.error(err.message, err);
   const status = err.status || 500;
   res.status(status).send({ message: err.message });
 };
