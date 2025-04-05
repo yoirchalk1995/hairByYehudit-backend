99df42c175c29c356a42d85ba5425619640b86d6
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-05 22:20:25 +0300

    added logger

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 require("express-async-errors");
 const helmet = require("helmet");
+require("./utils/logger");
 const notFound = require("./middleware/notFound");
 
 const services = require("./routes/services");
