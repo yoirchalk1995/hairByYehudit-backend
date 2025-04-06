ca1f980d746d07165cc2ef6e0d63a8b634ddd16e
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-06 22:34:39 +0300

    removed functionality to calculate endDate

--- utils/calcEndTime.js
+++ utils/calcEndTime.js
@@ -7,16 +7,21 @@
 function calcEndTime(date, startTime, lengthInMin) {
   let [year, month, day] = date.split("-");
   let [hour, minute] = startTime.split(":");
+
   let jsDate = new Date(year, month - 1, day, hour, minute);
+
   jsDate.setMinutes(jsDate.getMinutes() + lengthInMin);
+  if (jsDate.getDate !== Number(day))
+    throw Error("Appointment can not span two days");
+
   year = String(jsDate.getFullYear());
   month = String(jsDate.getMonth() + 1).padStart(2, "0");
   day = String(jsDate.getDate()).padStart(2, "0");
   hour = String(jsDate.getHours()).padStart(2, "0");
   minute = String(jsDate.getMinutes()).padStart(2, "0");
-  const endDate = `${year}-${month}-${day}`;
+
   const endTime = `${hour}:${minute}`;
-  return { endDate, endTime };
+  return { endTime };
 }
 
 module.exports = calcEndTime;
