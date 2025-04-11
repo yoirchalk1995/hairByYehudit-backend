5c328fcd1baba8034799866ac6b7ed78d8112681
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-11 04:09:58 +0300

    fixed

--- routes/users.js
+++ routes/users.js
@@ -22,67 +22,60 @@ router.post("/", async (req, res) => {
   let { email, userName, contactNumber, password, isAdmin, requestsVoice } =
     req.body;
 
-  try {
-    if (email) {
-      email = normalizeEmail(email);
-      const user = await getUserByColumn("email", email);
-      if (user.length)
-        return res.status(400).send("email address already in use");
-    }
-
-    if (userName) {
-      const usernameRows = await getUserByColumn("username", userName);
-      const contactNumberRows = await getUserByColumn(
-        "contact_number",
-        contactNumber
-      );
-      if (usernameRows.length || contactNumberRows.length)
-        return res
-          .status(400)
-          .send("username and contact number must be unique");
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const result = await insertUser(
-      ["username", "email", "contact_number", "hash", "is_admin"],
-      [userName, email, contactNumber, hash, isAdmin]
-    );
-
-    let sentEmail = false;
-    if (email) {
-      sendEmail(email, result.insertId);
-      sentEmail = true;
-    }
-
-    if (contactNumber && !sentEmail) {
-      const updatedNumber = interNumber(contactNumber);
-
-      if (requestsVoice) {
-        sendVoiceMessage(updatedNumber);
-        return;
-      }
-
-      const otp = await sendSMS(updatedNumber);
-
-      await db.query("INSERT INTO verifications (user_id, otp) VALUES (?,?)", [
-        result.insertId,
-        otp,
-      ]);
-    }
-
-    res.send({
-      userId: result.insertId,
-      email,
-      userName,
-      contactNumber,
-      isAdmin,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
+  if (email) {
+    email = normalizeEmail(email);
+    const user = await getUserByColumn("email", email);
+    if (user.length)
+      return res.status(400).send("email address already in use");
   }
+
+  if (userName) {
+    const usernameRows = await getUserByColumn("username", userName);
+    const contactNumberRows = await getUserByColumn(
+      "contact_number",
+      contactNumber
+    );
+    if (usernameRows.length || contactNumberRows.length)
+      return res.status(400).send("username and contact number must be unique");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const result = await insertUser(
+    ["username", "email", "contact_number", "hash", "is_admin"],
+    [userName, email, contactNumber, hash, isAdmin]
+  );
+
+  let sentEmail = false;
+  if (email) {
+    sendEmail(email, result.insertId);
+    sentEmail = true;
+  }
+
+  if (contactNumber && !sentEmail) {
+    const updatedNumber = interNumber(contactNumber);
+
+    if (requestsVoice) {
+      sendVoiceMessage(updatedNumber);
+      return;
+    }
+
+    const otp = await sendSMS(updatedNumber);
+
+    await db.query("INSERT INTO verifications (user_id, otp) VALUES (?,?)", [
+      result.insertId,
+      otp,
+    ]);
+  }
+
+  res.send({
+    userId: result.insertId,
+    email,
+    userName,
+    contactNumber,
+    isAdmin,
+  });
 });
 
 router.get("/", async (req, res) => {
@@ -93,7 +86,6 @@ router.get("/", async (req, res) => {
       _.isNull
     ));
   });
-  console.log(cleanedUsers);
 
   res.send(cleanedUsers);
 });
