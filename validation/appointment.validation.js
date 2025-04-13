454a5bafc6460984e011c3cc064deff57974b7f6
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-14 01:46:36 +0300

    changed Joi error message for invalid startTime format

--- validation/appointment.validation.js
+++ validation/appointment.validation.js
@@ -5,7 +5,11 @@ function validateAppointment(appointment) {
     userId: Joi.number().required().max(999999999),
     serviceId: Joi.number().required().max(999),
     date: Joi.date().required(),
-    startTime: Joi.string().regex(/^(?:[01]\d|2[0-3]):[0-5]\d$/),
+    startTime: Joi.string()
+      .regex(/^(?:[01]\d|2[0-3]):[0-5]\d$/)
+      .message(
+        "startTime must be in format hh:mm with 0 placeholder when necessary"
+      ),
   });
 
   return schema.validate(appointment);
